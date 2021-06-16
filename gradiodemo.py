ee0f15e92c0a4a351c90a72e7cf052e54cee4358
AK391 <81195143+AK391@users.noreply.github.com>
2021-06-16 15:02:30 -0400

    update link

--- gradiodemo.py
+++ gradiodemo.py
@@ -3,7 +3,7 @@ import gradio as gr
 
 
 description = "demo for HuBERT. To use it, simply add your audio or click one of the examples to load them. Read more at the links below."
-article = "<p style='text-align: center'><a href='https://arxiv.org/abs/2004.05150'>HuBERT: Self-Supervised Speech Representation Learning by Masked Prediction of Hidden Units</a> | <a href='https://github.com/pytorch/fairseq/tree/master/examples/hubert'>Github Repo</a></p>"
+article = "<p style='text-align: center'><a href='https://arxiv.org/abs/2106.07447'>HuBERT: Self-Supervised Speech Representation Learning by Masked Prediction of Hidden Units</a> | <a href='https://github.com/pytorch/fairseq/tree/master/examples/hubert'>Github Repo</a></p>"
 
 gr.Interface.load("huggingface/facebook/hubert-large-ls960-ft", 
     description=description,
